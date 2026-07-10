cb4b76aef6b7306a121093fcda9fbeaebba9dc5e
root <root@vpsk5nv3zatm.vps.cloudonfire.com>
2026-07-10 12:02:45 +0000

    Fix Chaquopy init and PlaybackService manifest

--- app/src/main/java/com/suspended/app/SuspendedApp.kt
+++ app/src/main/java/com/suspended/app/SuspendedApp.kt
@@ -1,7 +1,16 @@
 package com.suspended.app
 
 import android.app.Application
+import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SuspendedApp : Application()
+class SuspendedApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (!Python.isStarted()) {
+            Python.start(AndroidPlatform(this))
+        }
+    }
+}
